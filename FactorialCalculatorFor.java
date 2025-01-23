3aaba1643bb13e1020019fa70555c46b526a3d53
puneet0410 <puneetp2200@gmail.com>
2025-01-23 20:52:28 +0530

    Factorial Calculation.java

---
+++ FactorialCalculatorFor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FactorialCalculatorFor{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+        
+        if (n < 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            System.out.println("The factorial of " + n + " is: " + factorial);
+        }
+        
+        scanner.close();
+    }
+}
