e1a773d04822b413f85f992bdad7c21e4c0842fc
puneet0410 <puneetp2200@gmail.com>
2025-01-23 21:02:44 +0530

    Assignment 3

---
+++ MultiplicationTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        
+        System.out.println("Multiplication table of " + number + ":");
+        
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+        
+        
+    }
+}
